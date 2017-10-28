9126ffe76a5448caff834e44f171b487df0bb7ff
Aditya Emani <prasaadem@gmail.com>
2017-10-28 11:01:14 -0500

    Calendar, location implemented.

--- DragonFly Project/DetailViewController.swift
+++ DragonFly Project/DetailViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 import MapKit
+import EventKit
 
 class DetailViewController: UIViewController {
     
     var event:NSDictionary = NSDictionary()
     var eventImage:UIImage = UIImage()
     var eventId:NSString = ""
+    let eventStore = EKEventStore()
+    var calendar: EKCalendar!
+    var locationString:String = ""
     
     @IBOutlet weak var eventName: UILabel!
     @IBOutlet weak var eventDescription: UITextView!
@@ -21,6 +25,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var mapVIew: MKMapView!
     @IBOutlet weak var eventDate: UILabel!
     @IBOutlet weak var eventImageView: UIImageView!
+    @IBOutlet weak var respondToEvent: UIButton!
     
     func configureView() {
         // Update the user interface for the detail item.
@@ -41,7 +46,7 @@ class DetailViewController: UIViewController {
             let city:String = (location["city"] as? String)!
             let state:String = (location["state"] as? String)!
             
-            let locationString = "\(name), \(address), \(city), \(state)"
+            locationString = "\(name), \(address), \(city), \(state)"
             eventLocation.text = locationString
             
             eventDate.text = "10-02-2017"
@@ -87,6 +92,98 @@ class DetailViewController: UIViewController {
             
             self.mapVIew.addAnnotation(point)
         }
+    }
+    
+    @IBAction func addEvent(_ sender: Any) {
+        let status = EKEventStore.authorizationStatus(for: EKEntityType.event)
+        
+        switch (status) {
+        case EKAuthorizationStatus.notDetermined:
+            requestAccessToCalendar()
+        case EKAuthorizationStatus.authorized:
+            saveEvent()
+        case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
+            let alertController = UIAlertController(title: "Access Denied", message: "Go to settings and allow access to calendar", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+    }
+    
+    func requestAccessToCalendar() {
+        eventStore.requestAccess(to: EKEntityType.event, completion: {
+            (accessGranted: Bool, error: Error?) in
+            if accessGranted == true {
+                self.saveEvent()
+            }
+        })
+    }
+    
+    func saveEvent(){
+        print(getCalendar() as Any)
+        let newEvent = EKEvent(eventStore: eventStore)
+        
+        newEvent.calendar = getCalendar()
+        
+        newEvent.title = event["name"] as? String
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = (event["date"] as! String)
+        
+        let dateSubstring = dateString.split(separator: "T", maxSplits: 3, omittingEmptySubsequences: true)
+//        let date = dateFormatter.date(from: dateSubstring.s)
+        
+        print(dateSubstring as Any)
+//        newEvent.startDate = date
+//        newEvent.endDate = date
+        newEvent.isAllDay = true
+        
+        newEvent.location = locationString
+        
+        do {
+            try eventStore.save(newEvent, span: .thisEvent, commit: true)
+            let alert = UIAlertController(title: "Event saved", message: "Successfully", preferredStyle: .alert)
+            let dismiss = UIAlertAction(title: "dismiss", style: .cancel, handler: nil)
+            alert.addAction(dismiss)
+            self.present(alert, animated: true, completion: nil)
+            self.dismiss(animated: true, completion: nil)
+        } catch {
+            let alert = UIAlertController(title: "Event could not save", message: (error as NSError).localizedDescription, preferredStyle: .alert)
+            let dismiss = UIAlertAction(title: "dismiss", style: .cancel, handler: nil)
+            alert.addAction(dismiss)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getCalendar() -> EKCalendar? {
+        let defaults = UserDefaults.standard
+        
+        if let id = defaults.string(forKey: "calendarID") {
+            return eventStore.calendar(withIdentifier: id)
+        } else {
+            let calendar = EKCalendar(for: .event, eventStore: self.eventStore)
+            
+            calendar.title = "DragonFly"
+            calendar.cgColor = UIColor.red.cgColor
+            calendar.source = eventStore.defaultCalendarForNewEvents?.source
+            do{
+                try eventStore.saveCalendar(calendar, commit: true)
+            }catch{
+                
+            }
+            
+            defaults.set(calendar.calendarIdentifier, forKey: "calendarID")
+            
+            return calendar
+        }
+    }
+    
+    @IBAction func respondToEvent(_ sender: Any) {
+        
     }
 }
 
